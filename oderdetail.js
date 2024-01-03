738752250111fd830a8e973845b5d320ebf0a47e
rani10245kumari <ranik10245@gmail.com>
2024-01-03 19:57:39 +0530

    35

--- oderdetail.js
+++ oderdetail.js
@@ -3,35 +3,35 @@
 
 // amit -key
 const stripe = require('stripe')("sk_test_51OJiJgSHqKSNDjqq2yvXq0QFAEs76tHus5ZritXlXzN3LZU4FIX14dYNZt8am0foTMeYSugk8R9ktG8pMiQgOeIq00x0Vo0URC")
-const checkout = async(req,res)=>{
+const checkout = async (req, res) => {
     try {
-       const lineItems=  req.body.map(item=>{
+        const lineItems = req.body.map(item => {
             return {
-                price_data:{
-                    currency:"inr",
-                    product_data:{
-                        name:item.name
+                price_data: {
+                    currency: "inr",
+                    product_data: {
+                        name: item.name
                     },
-                    unit_amount:(item.price)*100
+                    unit_amount: (item.price) * 100
                 },
-                quantity:item.quantity
+                quantity: item.quantity
             }
 
         });
-        const session =await stripe.checkout.sessions.create({
-            payment_method_types:['card'],
-            mode:"payment",
-            line_items:lineItems,
-            success_url:"http://localhost:3001/payment/sucess",
-            cancel_url:"http://localhost:3001/payment/cancel"
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            mode: "payment",
+            line_items: lineItems,
+            success_url: "/",
+            cancel_url: "http://localhost:3001/payment/cancel"
         })
-        res.json({id:session.id})
-        
+        res.json({ id: session.id })
+
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
 
 }
 
 
-module.exports =checkout
+module.exports = checkout
